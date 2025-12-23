0bf26f348028254771e6384430855b9bb8f6592d
Sifat Ullah Chowdhury <sif.sifat24@gmail.com>
2025-12-24 01:31:01 +0600

    test: Write widget tests for dolfin test app

--- apps/dolfin_test/test/widget_test.dart
+++ apps/dolfin_test/test/widget_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 import 'package:dolfin_test/main.dart';
